7cb950e95f08155885f8254730186f18a9079240
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2025-04-09 17:17:48 -0400

    Contador de 1 a 10

---
+++ src/ContatorWhile.java
@@ -0,0 +1,13 @@
+public class ContatorWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+        while(contador <= 10){
+            System.out.println(contador);
+            contador++;
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
